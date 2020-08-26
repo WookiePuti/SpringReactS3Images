4fa3203f2e7a2035cd461ec66aa45978f37404dc
Wookie9412 <lukasputon@gmail.com>
2020-08-26 12:40:57 +0200

    init

---
+++ src/test/java/com/wookie/demo/AwsDemoUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wookie.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDemoUploaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
